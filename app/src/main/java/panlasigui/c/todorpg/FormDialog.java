9f4740a91f4599958b2bfbb85b2ed0cfc14b22d2
Cameron Panlasigui <c.panlasigui@gmail.com>
2016-01-29 17:50:34 -0800

    first commit

---
+++ app/src/main/java/panlasigui/c/todorpg/FormDialog.java
@@ -0,0 +1,39 @@
+package panlasigui.c.todorpg;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+//import android.view.LayoutInflater;
+
+/**
+ * Created by Cameron on 1/29/2016.
+ */
+public class FormDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.form, null))
+                // Add action buttons
+                .setPositiveButton(R.string.create, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        FormDialog.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
+
