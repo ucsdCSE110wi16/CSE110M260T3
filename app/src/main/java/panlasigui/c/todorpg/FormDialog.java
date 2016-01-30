3e366ce3a293e31ce9a9096bb37c3900f1835174
Cameron Panlasigui <c.panlasigui@gmail.com>
2016-01-29 19:05:00 -0800

    Updated task form

--- app/src/main/java/panlasigui/c/todorpg/FormDialog.java
+++ app/src/main/java/panlasigui/c/todorpg/FormDialog.java
@@ -6,21 +6,39 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 //import android.view.LayoutInflater;
 
 /**
- * Created by Cameron on 1/29/2016.
+ * Wow what a cool program
  */
 public class FormDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.form, null);
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.skills_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+
+        // Get the layout inflater
+
 
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
-        builder.setView(inflater.inflate(R.layout.form, null))
+        builder.setView(view)
                 // Add action buttons
                 .setPositiveButton(R.string.create, new DialogInterface.OnClickListener() {
                     @Override
@@ -33,6 +51,8 @@ public class FormDialog extends DialogFragment {
                         FormDialog.this.getDialog().cancel();
                     }
                 });
+
+
         return builder.create();
     }
 }
