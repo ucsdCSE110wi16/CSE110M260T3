ecdf1ea1ec69c19667f9bbee4365c7124e8cbac4
vishuyelisetty <vishu.yelisetty@gmail.com>
2016-02-12 01:23:47 -0800

    Starting to add login

---
+++ app/src/main/java/panlasigui/c/todorpg/Account.java
@@ -0,0 +1,14 @@
+package panlasigui.c.todorpg;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vishu on 2/12/2016.
+ */
+public class Account {
+    /* Fields relevant to an account */
+    private static String username;
+    private static String password;
+    private ArrayList<TaskData> taskList;
+
+}
