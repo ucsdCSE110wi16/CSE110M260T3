2df6c9f41790e95911ba5ca8b9cd4e16da66ff38
Thomas Qian <tom07qian@gmail.com>
2016-03-11 23:51:57 -0800

    update createtasktest and add edittasktest

--- app/src/androidTest/java/panlasigui/c/todorpg/EditTaskTest.java
+++ app/src/androidTest/java/panlasigui/c/todorpg/EditTaskTest.java
@@ -1,32 +1,26 @@
 package panlasigui.c.todorpg;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import android.content.ClipData;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
-import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import panlasigui.c.todorpg.Activities.LoginActivity;
 import panlasigui.c.todorpg.Activities.TaskPage;
 import panlasigui.c.todorpg.Classes.TaskData;
 
 import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.core.Is.is;
 
@@ -37,25 +31,21 @@ import static org.hamcrest.core.Is.is;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class CreateTaskTest {
+public class EditTaskTest {
     private String inputUsername;
     private String inputPassword;
     private String taskName;
     private String taskDesc;
-    private String taskCat;
-    private float taskDif;
 
     @Rule
     public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(
             LoginActivity.class);
     @Before
     public void initInput() {
-        this.inputUsername = "CreateTaskTest@tester.com";
-        this.inputPassword = "CreateTask";
-        taskName = "Testing Task";
-        taskDesc = "This is a test!";
-        taskCat = "Intelligence";
-        taskDif = (float)3.0;
+        this.inputUsername = "EditTaskTest@test.com";
+        this.inputPassword = "EditTask";
+        taskName = "Editing Task";
+        taskDesc = "This was edited!";
     }
 
     @Test
@@ -67,21 +57,43 @@ public class CreateTaskTest {
         while(TaskPage.taskList == null) {}
         int size = TaskPage.taskList.size();
 
-        //create task
-        onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.taskName)).perform(typeText(this.taskName));
-        onView(withId(R.id.taskDescription)).perform(typeText(this.taskDesc), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.taskCategorySpinner)).perform(click());
-        onData(allOf(is(instanceOf(String.class)), is(taskCat))).perform(click());
-        onView(withId(R.id.taskCategorySpinner)).perform(click());
-        onView(withId(R.id.difficultyBar)).perform(click());
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //edit task
+        onData(anything()).inAdapterView(withId(R.id.lvItems)).atPosition(0).perform(click());
+        onView(withId(R.id.taskName)).perform(clearText(), typeText(this.taskName));
+        onView(withId(R.id.taskDescription)).perform(clearText(),
+                typeText(this.taskDesc), ViewActions.closeSoftKeyboard());
         onView(withId(android.R.id.button1)).perform(click());
 
-        while(TaskPage.taskList.size() == size) {}
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         TaskData data = TaskPage.taskList.get(0);
         assertThat(data.getName(), is(this.taskName));
         assertThat(data.getDescription(), is(this.taskDesc));
-        assertThat(data.getCategory(), is(this.taskCat));
-        assertThat(data.getDifficulty(), is(this.taskDif));
+
+        onData(anything()).inAdapterView(withId(R.id.lvItems)).atPosition(0).perform(click());
+        onView(withId(R.id.taskName)).perform(clearText(), typeText("reverted"));
+        onView(withId(R.id.taskDescription)).perform(clearText(),
+                typeText("reverted"), ViewActions.closeSoftKeyboard());
+        onView(withId(android.R.id.button1)).perform(click());
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        data = TaskPage.taskList.get(0);
+        assertThat(data.getName(), is("reverted"));
+        assertThat(data.getDescription(), is("reverted"));
     }
 }
